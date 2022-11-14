5b21af2288608800250d8cab84ea0613bff01e84
Baptiste Azevedo <azevedo.baptiste@orange.fr>
2022-11-14 20:38:58 +0100

    Ajout page About

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import Chevron from '../assets/images/chevron.svg'
+import React, { useState, useRef, useEffect } from 'react';
 
-const Collapse = () => {
+const Collapse = (props) => {
+
+    const [toggle, setToggle] = useState(false);
+    const [heightEl, setHeightEl] = useState();
+
+    const toggleState = () => {
+        setToggle(!toggle);
+    }
+
+    const refHeight = useRef();
+
+    useEffect(() => {
+        setHeightEl(`${refHeight.current.scrollHeight}px`)
+    }, [])
+
     return (
-        <div className='Collapse'>
-
+        <div className="collapse">
+            <div
+                onClick={toggleState}
+                className="collapse-visible">
+                <h2>{props.title}</h2>
+                <img
+                    src={Chevron}
+                    alt="chevron"
+                    className={toggle ? "chevron chevron-rotate" : "chevron"} />
+            </div>
+            <div
+                ref={refHeight}
+                className={
+                    toggle ? "collapse-toggle animated" : "collapse-toggle"
+                }
+                style={{ height: toggle ? `${heightEl}` : "0px" }}
+            >
+                <p
+                    aria-hidden={toggle ? "true" : "false"}
+                    className="description "
+                >
+                    {props.description}</p>
+            </div>
         </div>
     );
 };
